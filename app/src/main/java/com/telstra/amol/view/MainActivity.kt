578eab8605beb0fbcd23c96c9a744ab3448edb8f
Amol <amolkhose15@gmail.com>
2020-04-01 18:40:48 +0530

    Repository Updated

--- app/src/main/java/com/telstra/amol/view/MainActivity.kt
+++ app/src/main/java/com/telstra/amol/view/MainActivity.kt
@@ -6,6 +6,7 @@ import com.telstra.amolassignmenttestra.R
 
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_actvitymain)
